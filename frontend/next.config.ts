56e7eb9eb45c55dd39c209c1cc9f2955f54567c3
Vedant <48868398+vedantb2@users.noreply.github.com>
2025-12-02 16:44:38 +0000

    .

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   experimental: {
-    turbopackFileSystemCacheForBuild: true,
+    // turbopackFileSystemCacheForBuild: true,
     turbopackFileSystemCacheForDev: true,
   },
 };
