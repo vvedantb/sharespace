0f318186365fd4144717ec223998c75b34a49f70
Vedant <48868398+vedantb2@users.noreply.github.com>
2025-12-30 20:35:45 +0000

    add student request dto

--- backend/student-service/src/main/java/com/sharespace/studentservice/service/StudentService.java
+++ backend/student-service/src/main/java/com/sharespace/studentservice/service/StudentService.java
@@ -1,15 +1,16 @@
 package com.sharespace.studentservice.service;
 
+import com.sharespace.studentservice.mapper.StudentMapper;
 import com.sharespace.studentservice.model.Student;
 import com.sharespace.studentservice.repository.StudentRepository;
 import org.springframework.stereotype.Service;
-import com.sharespace.studentservice.dto.StudentResponseDTO
+import com.sharespace.studentservice.dto.StudentResponseDTO;
 
 import java.util.List;
 
 @Service
 public class StudentService {
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -17,5 +18,9 @@ public class StudentService {
 
     public List<StudentResponseDTO> getStudents (){
         List<Student> students = studentRepository.findAll();
+
+        List<StudentResponseDTO> studentResponseDTOs = students.stream().map(student -> StudentMapper.toDTO(student)).toList();
+
+        return studentResponseDTOs;
     }
 }
