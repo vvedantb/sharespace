8cc337a20f8ef1b7a055113e086af7ebd34d9f13
Vedant <48868398+vedantb2@users.noreply.github.com>
2026-01-02 00:22:15 +0000

    .

---
+++ backend/sharespace-api/src/main/java/com/sharespace/ShareSpaceApplication.java
@@ -0,0 +1,12 @@
+package com.sharespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareSpaceApplication.class, args);
+    }
+}
