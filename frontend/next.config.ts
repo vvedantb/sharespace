75256034e66f1d47c91dffdab28d2a5cbd80e837
Vedant <48868398+vedantb2@users.noreply.github.com>
2026-01-03 23:15:15 +0000

    .

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
